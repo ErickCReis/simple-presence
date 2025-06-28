842787f3eca6f4518bbc474fb6a2ae372aaa9eb3
Erick Reis <erickcorreareis@gmail.com>
2025-06-28 15:04:19 -0300

    drizzle

---
+++ apps/server/drizzle-prod.config.ts
@@ -0,0 +1,20 @@
+import { readFileSync } from "node:fs";
+import { defineConfig } from "drizzle-kit";
+
+const db = readFileSync(
+	".alchemy/simple-presence/erickreis/simple-presence-db.json",
+	{ encoding: "utf8" },
+);
+const dbJson = JSON.parse(db);
+const { id: databaseId, accountId } = dbJson.output;
+
+export default defineConfig({
+	schema: "./src/db/schema",
+	dialect: "sqlite",
+	driver: "d1-http",
+	dbCredentials: {
+		accountId: accountId,
+		databaseId: databaseId,
+		token: process.env.CLOUDFLARE_API_TOKEN!,
+	},
+});
--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,17 +1,19 @@
+import { readdirSync } from "node:fs";
+import { join } from "node:path";
 import { defineConfig } from "drizzle-kit";
+
+const alchemyD1Path = "./.alchemy/miniflare/d1/miniflare-D1DatabaseObject";
+const files = readdirSync(alchemyD1Path);
+const dbPath = files.find((file) => file.endsWith(".sqlite"));
+
+if (!dbPath) {
+	throw new Error("No database file found");
+}
 
 export default defineConfig({
 	schema: "./src/db/schema",
-	out: "./src/db/migrations",
-	// DOCS: https://orm.drizzle.team/docs/guides/d1-http-with-drizzle-kit
 	dialect: "sqlite",
-	// driver: "d1-http",
-	// dbCredentials: {
-	// 	accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-	// 	databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-	// 	token: process.env.CLOUDFLARE_D1_TOKEN!,
-	// },
 	dbCredentials: {
-		url: "./.wrangler/state/v3/d1/miniflare-D1DatabaseObject/f26dd3b4af2b86035a68fbb491403d4b54d63dee4c1e42d18d4ad58dc6978866.sqlite",
+		url: join(alchemyD1Path, dbPath),
 	},
 });
